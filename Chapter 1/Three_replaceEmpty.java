c7dc51c3d06fa8d17c8ef1faf194b2aa060604ba
XIAOLONG LI <1491skdpf89@gmail.com>
2019-11-28 16:19:43 -0500

    Update Three_replaceEmpty.java

--- Chapter 1/Three_replaceEmpty.java
+++ Chapter 1/Three_replaceEmpty.java
@@ -1,19 +1,25 @@
 class Three_replaceEmpty {
 	public static void main(String[] args) {
-		
+		  
 		String s = "I love NYC";
+		String s1 = "I love NYC ";
 		System.out.println(replace(s));
+
+		System.out.println(replace(s).length());
+		System.out.println(replace(s1).length());
+
 
 	}
 	private static String replace(String s) {
 
 		StringBuilder sb = new StringBuilder();
 
-		for(char ch: s.toCharArray()) {
-			if(ch == ' '){
+		for(int i = 0; i < s.length(); i++) {
+			char ch = s.charAt(i);
+			if(ch == ' ' && i != s.length() - 1){
 				sb.append("%20");
 			}
-			else{
+			else if (ch != ' '){
 				sb.append(ch);
 			}
 		}
