eea6672d35273fc70d6b767bfe57910a33158931
XIAOLONG LI <1491skdpf89@gmail.com>
2019-11-28 15:14:15 -0500

    Update One_isUnique.java

--- Chapter 1/One_isUnique.java
+++ Chapter 1/One_isUnique.java
@@ -1,17 +1,22 @@
 One_isUnique {
   
-  //O(n^2)
+  public static void main(String[] args) {
+	String s1 = "abcd";
+	String s2 = "abcc";
+	System.out.println(isUnique(s1));
+	System.out.println(isUnique(s2));
+
+}
+	//O(n^2)
   private static boolean isUnique(String s){
-		// check if a string has all unique characters, can't use addtional datastructure
-		for(int i = 0; i < s.length(); i++) {
-			for(int j = i + 1; j < s.length(); j++) {
-				if(s.charAt(i) == s.charAt(j)){
-					return false;
-
-				}
-			}
+       // check if a string has all unique characters, can't use addtional datastructure
+     for(int i = 0; i < s.length(); i++) {
+	 for(int j = i + 1; j < s.length(); j++) {
+		if(s.charAt(i) == s.charAt(j)){
+		      return false;
 		}
-		return true;
-	}
-
+	 }
+     }
+    return true;
+  }
 }
