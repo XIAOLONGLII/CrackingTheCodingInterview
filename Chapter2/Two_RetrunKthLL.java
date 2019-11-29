a6b90982a1c2825994b07981a6ebbf80e819f159
XIAOLONG LI <1491skdpf89@gmail.com>
2019-11-29 15:14:26 -0500

    Update Two_RetrunKthLL.java

--- Chapter2/Two_RetrunKthLL.java
+++ Chapter2/Two_RetrunKthLL.java
@@ -2,7 +2,36 @@
 2.2 Return Kth to Last: Implement an algorithm to find the kth to last element of a singly linked list. 
 */
 class Two_RetrunKthLL {
+     private static int theKthToLastLL(LinkedList list, int K) {
 
+        // k = 2, would be the last 2 
+        // 1 2 3 4 5-> 4
+        // find the length of linked list then - k + 1
+        Node current = list.head;
+        int listLength = 0;
+        while(current != null) {
+            listLength++;
+            current = current.next;
+        }
+
+        int location = listLength - K + 1;
+        Node cur = list.head;
+        int count = 0;
+        while(cur != null) {
+            count++;
+            if(count == location) {
+                return cur.data;
+            }
+            cur = cur.next;
+        }
+        return -1;
+
+    }
+    
+    
+    
+    
+    // bonus point: print all ll from K till end
     private static LinkedList KthToLastLL(LinkedList list, int K) {
         // return Kth to the last
         // k = 2, then 2 to the last
