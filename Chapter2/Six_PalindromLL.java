60cbb3025dcd05dc3310336f9e7a761494a59b1a
XIAOLONG LI <1491skdpf89@gmail.com>
2019-11-29 18:43:56 -0500

    Create Six_PalindromLL.java

---
+++ Chapter2/Six_PalindromLL.java
@@ -0,0 +1,29 @@
+class {
+  private static boolean isPalindromeLL(LinkedList list) {
+        // 1 -> 2 -> 3 -> 2 -> 1
+
+        StringBuilder sb = new StringBuilder();
+        Node current = list.head;
+        while(current != null) {
+            sb.append(current.data);
+            current = current.next;
+        }
+
+        System.out.println( "sb: "+sb);
+
+        return isPalindrome(sb.toString());
+       
+    }
+    private static boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+        while( left < right){
+            if(s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
