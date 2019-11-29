9e7e2efbeae0cc3b7f53f5ede031913652675f31
XIAOLONG LI <1491skdpf89@gmail.com>
2019-11-28 23:32:15 -0500

    Update Two_isPermutation.java

--- Chapter 1/Two_isPermutation.java
+++ Chapter 1/Two_isPermutation.java
@@ -1,3 +1,8 @@
+/*
+1.2 Check Permutation: Given two strings, write a method to decide if one is a permutation of the
+other. 
+*/
+
 import java.util.*;
 class Two_isPermutation {
 	public static void main(String[] args) {
