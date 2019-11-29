1187379a137dbde4e10456725da56058284c2c1a
XIAOLONG LI <1491skdpf89@gmail.com>
2019-11-29 16:27:56 -0500

    Update Three_DeleteMiddleNode.java

--- Chapter2/Three_DeleteMiddleNode.java
+++ Chapter2/Three_DeleteMiddleNode.java
@@ -1,3 +1,24 @@
 class Three_DeleteMiddleNode {
 
+  private static LinkedList deleteMiddleNode(LinkedList list) {
+        // a - >b- >c - >d - >e- >f 
+        // a - >b- >d - >e- >f 
+        Node walker = list.head;
+        Node runner = list.head;
+        LinkedList afterDelete = new LinkedList();
+        while(runner.next != null && runner.next.next != null) {
+            walker = walker.next;
+            runner = runner.next.next;
+        }
+
+        Node current = list.head;
+
+        while(current != null) {
+            if(current != walker){
+                afterDelete = insert(afterDelete, current.data);
+            } 
+            current = current.next;
+        }
+        return afterDelete;
+    }
 }
