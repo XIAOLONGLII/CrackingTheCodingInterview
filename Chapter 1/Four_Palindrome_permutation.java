65b7a602086fb8354be48d0c221c90b32822a134
XIAOLONG LI <1491skdpf89@gmail.com>
2019-11-28 23:31:19 -0500

    Update Four_Palindrome_permutation.java

--- Chapter 1/Four_Palindrome_permutation.java
+++ Chapter 1/Four_Palindrome_permutation.java
@@ -1,3 +1,11 @@
+/*
+1.4 Palindrome Permutation: Given a string, write a function to check if it is a permutation of a palindrome. A palindrome is a word or phrase that is the same forwards and backwards. A permutation
+is a rearrangement of letters. The palindrome does not need to be limited to just dictionary words.
+EXAMPLE
+Input: Tac t Coa
+Output: Tru e (permutations : "tac o cat" , "atc o eta" , etc. ) 
+
+*/
 import java.util.*;
 class Four_Palindrome_permutation {
 	public static void main(String[] args) {
