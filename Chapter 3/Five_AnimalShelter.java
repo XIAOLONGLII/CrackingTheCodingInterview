91e24941e2a3fb5a64ba070018a8927dac5aad82
XIAOLONG LI <1491skdpf89@gmail.com>
2019-11-30 14:35:44 -0500

    Update Five_AnimalShelter.java

--- Chapter 3/Five_AnimalShelter.java
+++ Chapter 3/Five_AnimalShelter.java
@@ -1,4 +1,55 @@
 class Five_AnimalShelter {
+	// 1. FIFO(queue)
+	// 2. choose cat or dog
+	public static void main(String[] args) {
+		Queue<String> Cats = new LinkedList<>();
+		Queue<String> Dogs = new LinkedList<>();
+
+		Cats.add("A");
+		Cats.add("B");
+		Cats.add("C");
+		Cats.add("D");
+		Cats.add("E");
+
+		Dogs.add("a");
+		Dogs.add("b");
+		Dogs.add("c");
+		Dogs.add("d");
+		Dogs.add("e");
+		System.out.println(Cats);
+		System.out.println(Dogs);
+
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println("Would you like to adpot a dog or cat? ");
+		String catOrDog = keyboard.next();
+		if(catOrDog.equals("dog")) {
+			System.out.println(dequeueDog(Dogs));
+		}
+		if(catOrDog.equals("cat")) {
+			System.out.println(dequeueCat(Cats));
+		}
+
+	}
+	private static String dequeueDog(Queue<String> Dogs){
+		if(!Dogs.isEmpty()){
+			return Dogs.remove();
+		}
+		else{
+			System.out.println("There are no more available dogs");
+			return " ";
+		}
+	}
+
+	private static String dequeueCat(Queue<String> Cats){
+		if(!Cats.isEmpty()){
+			return Cats.remove();
+		}
+		else{
+			System.out.println("There are no more available cats");
+			return " ";
+		}
+		
+	}
 
 
 
