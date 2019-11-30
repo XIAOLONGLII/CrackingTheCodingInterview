0b53bba350e38a1d2f55c9427c76339b4bb6cbab
XIAOLONG LI <1491skdpf89@gmail.com>
2019-11-30 13:09:27 -0500

    Update Two_StackMin.java

--- Chapter 3/Two_StackMin.java
+++ Chapter 3/Two_StackMin.java
@@ -3,6 +3,33 @@
 which returns the minimum eiement? Push, pop and min should ail operate in 0(1 ) time. 
 */
 // how to implement a stack
+
+public class StackwithMin extends Stack<NodeWithMin> {
+		public void push(int value) {
+			int newMin = Math.min(value, min());
+			super.push(new NodeWithMin(value, newMin));
+		}
+		public int min(){
+			if(this.isEmpty()){
+				return Integer.MAX_VALUE;
+			}
+			else {
+				return peek().min;
+			}
+		}
+	}
+	
+	class NodeWithMin{
+	public int value;
+	public int min;
+	public NodeWithMin(int value, int min) {
+		this.value = value;
+		this.min = min;
+	}
+}
+
+
+
 class Stack {
 	StackNode root;
 	
