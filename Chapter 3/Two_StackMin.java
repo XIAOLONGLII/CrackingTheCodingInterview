9fe75b9ca0823c9badcbc03fd31f2c69efb2744a
XIAOLONG LI <1491skdpf89@gmail.com>
2019-11-30 12:25:22 -0500

    Create Two_StackMin.java

---
+++ Chapter 3/Two_StackMin.java
@@ -0,0 +1,8 @@
+/*
+3.2 Stack Min: How would you design a stack which, in addition to push and pop, has a function min
+which returns the minimum eiement? Push, pop and min should ail operate in 0(1 ) time. 
+*/
+class Two_StackMin {
+  
+
+}
