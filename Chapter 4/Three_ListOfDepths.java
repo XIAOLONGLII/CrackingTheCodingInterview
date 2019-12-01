fa80f7d24d3bca214c4131a1c8ff4c66cbe1edb6
XIAOLONG LI <1491skdpf89@gmail.com>
2019-12-01 16:53:45 -0500

    Update Three_ListOfDepths.java

--- Chapter 4/Three_ListOfDepths.java
+++ Chapter 4/Three_ListOfDepths.java
@@ -3,6 +3,29 @@ at each depth (e.g., if you have a tree with depth D, you'll have D linked lists
 */
 
 class Three_ListOfDepths {
+  private static List<List<Integer>> depthOfList(Node root, List<List<Integer>> lists, int level) {
+
+      if(root == null) return null;
+      List<Integer> list = null;
+      if(list.size() == level){
+        list = new LinkedList<Node>();
+        lists.add(list);
+      }
+      else {
+        list = lists.get(level);
+      }
+      list.add(level);
+      list.add(root);
+
+      depthOfList(root.left, lists, level + 1);
+      depthOfList(root.right, lists, level + 1);
+    }
+    
+  List<List<Integer>> depthOfList(Node root) {
+        List<List<Integer>> lists = new ArrayList<LinkedList<Node>>();
+        depthOfList(root, lists, 0);
+        return list;
+  }
 
 
 }
