0cf06967b82f80003bea0789e48039427730f6da
XIAOLONG LI <1491skdpf89@gmail.com>
2019-12-01 15:06:10 -0500

    Create Two_CreateBST.java

---
+++ Chapter 4/Two_CreateBST.java
@@ -0,0 +1,8 @@
+/*
+ Minimal Tree: Given a sorted (increasing order) array with unique integer elements, write an
+algorithm to create a binary search tree with minima! height. 
+
+*/
+class Two_CreateBST{
+
+}
