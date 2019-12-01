f6dc32412e60eca8128716c700fc64159638b907
XIAOLONG LI <1491skdpf89@gmail.com>
2019-12-01 14:47:46 -0500

    Update One_Route.java

--- Chapter 4/One_Route.java
+++ Chapter 4/One_Route.java
@@ -3,7 +3,39 @@
 route between two nodes. 
 */
 class One_Route {
-
+	enum State {
+		Unvisited, Visited, Visiting;
+	}
+	boolean search(Graph graph, Node start, Node end) {
+		if(start == end) return true;
+	}
+	// Queue
+	LinkedList<Node> q = new LinkedList<>();
+	for(Node u: graph.getNodes()){
+		u.state = State.Unvisited;
+	}
+	start.state = State.Visiting;
+	q.add(start);
+	Node u;
+	while(!q.isEmpty()) {
+		u = q.removeFirst();
+		if(u != null) {
+			for(Node v: u.getAdjacent()) {
+				if(v.state == State.Unvisited) {
+					if(v == end) {
+						return true;
+					}
+					else {
+						v.state = State.Visiting;
+						q.add(v);
+					}
+				}
+			}
+			u.state = State.Visited;
+		}
+	}
+	return false;
+}
 
 
 }
